c58f3b4393a1c6344e1e054ebfab919d61397485
王涛 <tao.wang.go@gmail.com>
2026-03-18 11:13:15 +0100

    game music and sound.

---
+++ SumQuest/Assets/Scripts/Main.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour
+{
+    [SerializeField] private Button InitialNumber;
+    [SerializeField] private Image InitialTarget;
+    [SerializeField] private GameObject InitialEffect;
+    [SerializeField] private List<Image> InitialImages;
+    [SerializeField] private GameObject GameResult;
+    [SerializeField] private Button Next;
+    
+    private Dictionary<int, int> Numbers = new();
+    private Dictionary<int, Button> Buttons = new();
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Config.Initialize();
+        InitialNumber.gameObject.SetActive(false);      
+        GameResult.gameObject.SetActive(false);
+        Next.onClick.AddListener(() => NextLevel());
+          
+        GameNumbers.Initialize(InitialImages);
+        Effect.Initialize(InitialEffect);
+
+        DrawNumbers();
+        Header.Initialize(InitialTarget);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NextLevel()
+    {      
+        Config.NextLevel();
+        DrawNumbers();
+        Header.UpdateHeader();
+        GameResult.gameObject.SetActive(false);
+    }
+
+    private void DrawNumbers()
+    {
+        var startPosition = InitialNumber.transform.position;
+        var randomNumbers = Config.InitializeNumbers();
+        var numberIndex = 0;
+        for (int i = 0; i < Config.Width; ++i)
+        {
+            for (int j = 0; j < Config.Height; ++j)
+            {
+                var button = GetButtonObject();
+                button.GetComponent<RectTransform>().sizeDelta = new Vector2(Config.ButtonWidth, Config.ButtonHeight);
+                button.transform.position = 
+                    new Vector3(startPosition.x + (Config.ButtonWidth + Config.ButtonPadding) * i, 
+                    startPosition.y - (Config.ButtonHeight + Config.ButtonPadding) * j, startPosition.z);
+                button.transform.SetParent(InitialNumber.transform.parent);
+                button.name = Config.Index(i, j).ToString();
+                button.gameObject.SetActive(true);
+                var buttonIndex = Config.Index(i, j);
+                button.onClick.AddListener(() => OnButtonClick(buttonIndex));
+                Buttons[buttonIndex] = button;
+
+                var num = randomNumbers[numberIndex];
+                Numbers[buttonIndex] = num;
+                ++numberIndex;
+
+                GameNumbers.DrawNumber(buttonIndex, num, button.transform.position);
+            }
+        }
+    }
+
+    private Button GetButtonObject()
+    {
+        foreach (var o in Buttons.Values)
+        {
+            if (!o.IsActive())
+            {
+                return o;
+            }
+        }
+        return GameObject.Instantiate(InitialNumber);
+    }
+
+    private void OnButtonClick(int index)
+    {
+        // Debug.Log($"Click index: {index}");
+
+        SoundManager.Instance.PlayButtonClick();
+
+        if (Config.Select == -1)
+        {
+            Config.Select = index;
+            GameNumbers.DrawSelect(index, Buttons[index].transform.position);
+            return;
+        }
+        if (Config.Select == index)
+        {
+            Config.Select = -1;
+            GameNumbers.HideSelect();
+            return;
+        }
+        if (Numbers[Config.Select] + Numbers[index] == Config.Target)
+        {
+            SoundManager.Instance.PlayNumberMatch();
+
+            Buttons[Config.Select].gameObject.SetActive(false);
+            Buttons[Config.Select].onClick.RemoveAllListeners();
+            Buttons[index].gameObject.SetActive(false);
+            Buttons[index].onClick.RemoveAllListeners();
+            GameNumbers.HideNumer(Config.Select);
+            GameNumbers.HideNumer(index);
+            Effect.Play(Buttons[Config.Select].transform.position, Buttons[index].transform.position);
+            Config.Select = -1;
+            GameNumbers.HideSelect();           
+            Config.LeftNumberCount -= 2;
+            if (Config.LeftNumberCount <= 0)
+            {
+                GameResult.gameObject.SetActive(true);
+            }
+            return;
+        }
+        Config.Select = index;
+        GameNumbers.DrawSelect(index, Buttons[index].transform.position);
+    }  
+}
---
+++ SumQuest/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    private AudioSource musicSource;   // for background music
+    private AudioSource sfxSource;     // for sound effects
+
+    [Header("UI Sounds")]
+    [SerializeField] private AudioClip buttonClickSound;
+    [SerializeField] private AudioClip numberMatchSound;
+
+    [Header("Background Music")]
+    [SerializeField] private AudioClip backgroundMusic;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            // DontDestroyOnLoad(gameObject);
+
+            musicSource = gameObject.AddComponent<AudioSource>();
+            sfxSource = gameObject.AddComponent<AudioSource>();
+
+            musicSource.loop = true;
+            musicSource.playOnAwake = false;
+            musicSource.volume = 0.2f;
+
+            sfxSource.playOnAwake = false;
+
+            if (backgroundMusic != null)
+            {
+                PlayMusic(backgroundMusic);
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            musicSource.clip = clip;
+            musicSource.Play();
+            // Debug.LogWarning("Play background music");
+        }
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        musicSource.volume = volume;
+    }
+
+    private void PlaySFX(AudioClip clip)
+    {
+        if (clip != null)
+            sfxSource.PlayOneShot(clip);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        sfxSource.volume = volume;
+    }
+
+    public void PlayButtonClick()
+    {
+        PlaySFX(buttonClickSound);
+    }
+
+    public void PlayNumberMatch()
+    {
+        PlaySFX(numberMatchSound);
+    }
+}
